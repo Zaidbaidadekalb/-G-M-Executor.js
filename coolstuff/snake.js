70344d4621f902899afe8218d5fe323a13328cdf
Zaid Baida <87798207+Zaidbaidadekalb@users.noreply.github.com>
2022-05-04 15:01:49 -0500

    Create snake.js

---
+++ coolstuff/snake.js
@@ -0,0 +1,4 @@
+// p to play \\
+
+
+javascript:(function(){window.s0=document.createElement('script');window.s0.setAttribute('type','text/javascript');window.s0.setAttribute('src','https://bookmarkify.it/bookmarklets/50691/raw');document.getElementsByTagName('body')[0].appendChild(window.s0);})();
